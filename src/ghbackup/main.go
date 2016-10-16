ad5cfae9569a46c4d1ab9cd986636317b1b9f101
Amit Saha <amitsaha.in@gmail.com>
2016-10-16 19:40:19 +1100

    More work

--- src/ghbackup/main.go
+++ src/ghbackup/main.go
@@ -8,13 +8,15 @@ import (
 func main() {
     client := github.NewClient(nil)
     user := "amitsaha"
+    // Pagination?
     opt := &github.RepositoryListOptions{Type: "owner", Sort: "updated", Direction: "desc"}
     repos, _, err := client.Repositories.List(user, opt)
     if err != nil {
             fmt.Println(err)
     }
     for _, repo := range repos {
-        fmt.Printf("Name: %s GitURL: %s\n", repo.Name, repo.GitURL)
+        // Better way to print dereferenced pointers?
+        fmt.Printf("Name: %v GitURL: %v\n", *repo.Name, *repo.GitURL)
         //fmt.Println(repo.String())
     }
 }
