33dde28b1dbf2d54078686efdd8191da9eb609a6
Amit Saha <amitsaha.in@gmail.com>
2016-10-16 23:07:58 +1100

    Works

--- src/ghbackup/main.go
+++ src/ghbackup/main.go
@@ -2,21 +2,37 @@ package main
 
 import (
     "fmt"
+    "os/exec"
     "github.com/google/go-github/github"
 )
 
 func main() {
     client := github.NewClient(nil)
+    // Make these configurable
     user := "amitsaha"
-    // Pagination?
-    opt := &github.RepositoryListOptions{Type: "owner", Sort: "updated", Direction: "desc"}
-    repos, _, err := client.Repositories.List(user, opt)
-    if err != nil {
-            fmt.Println(err)
-    }
-    for _, repo := range repos {
-        // Better way to print dereferenced pointers?
-        fmt.Printf("Name: %v GitURL: %v\n", *repo.Name, *repo.GitURL)
-        //fmt.Println(repo.String())
+    repoType := "all"
+    opt := &github.RepositoryListOptions{Type: repoType, Sort: "created", Direction: "desc"}
+    for {
+        repos, resp, err := client.Repositories.List(user, opt)
+        if err != nil {
+                fmt.Println(err)
+        } else {
+            for _, repo := range repos {
+                // Better way to print dereferenced pointers?
+                fmt.Printf("Name: %v GitURL: %v\n", *repo.Name, *repo.GitURL)
+                cmd := exec.Command("git", "clone", *repo.GitURL)
+                err := cmd.Run()
+                if err != nil {
+                    fmt.Printf("Error cloning %v\n", *repo.Name)
+                }
+            }
+        }
+
+        // Learn about GitHub's pagination here:
+        // https://developer.github.com/v3/
+        if resp.NextPage == 0 {
+            break
+        }
+        opt.ListOptions.Page = resp.NextPage
     }
 }
