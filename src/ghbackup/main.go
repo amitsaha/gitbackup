d0ae4ffe25e191ce8c40bd386fb116354345abeb
Amit Saha <amitsaha.in@gmail.com>
2016-10-28 13:55:05 +1100

    More work

--- src/ghbackup/main.go
+++ src/ghbackup/main.go
@@ -1,38 +1,66 @@
 package main
 
 import (
-    "fmt"
-    "os/exec"
-    "github.com/google/go-github/github"
+	"fmt"
+	"github.com/google/go-github/github"
+	"log"
+	"os"
+	"os/exec"
 )
 
 func main() {
-    client := github.NewClient(nil)
-    // Make these configurable
-    user := "amitsaha"
-    repoType := "all"
-    opt := &github.RepositoryListOptions{Type: repoType, Sort: "created", Direction: "desc"}
-    for {
-        repos, resp, err := client.Repositories.List(user, opt)
-        if err != nil {
-                fmt.Println(err)
-        } else {
-            for _, repo := range repos {
-                // Better way to print dereferenced pointers?
-                fmt.Printf("Name: %v GitURL: %v\n", *repo.Name, *repo.GitURL)
-                cmd := exec.Command("git", "clone", *repo.GitURL)
-                err := cmd.Run()
-                if err != nil {
-                    fmt.Printf("Error cloning %v\n", *repo.Name)
-                }
-            }
-        }
+	client := github.NewClient(nil)
+	// Make these configurable
+	user := "amitsaha"
+	repoType := "all"
+	BACKUP_DIR := "/tmp/ghbackup"
+	opt := &github.RepositoryListOptions{Type: repoType, Sort: "created", Direction: "desc"}
+	for {
+		repos, resp, err := client.Repositories.List(user, opt)
+		if err != nil {
+			fmt.Println(err)
+		} else {
+			// default to ~/.ghbackup as the backup directory
+			_, err := os.Stat(BACKUP_DIR)
+			if err != nil {
+				fmt.Printf("%s doesn't exist, creating it\n", BACKUP_DIR)
+				err := os.Mkdir(BACKUP_DIR, os.ModeDir)
+				if err != nil {
+					log.Fatal(err)
+				}
+			}
+			err = os.Chdir(BACKUP_DIR)
+			for _, repo := range repos {
+				// Check if we have a copy of the repo already, if
+				// we do, we update the repo, else we do a fresh clone
+				_, err := os.Stat(*repo.Name)
+				if err == nil {
+					fmt.Printf("%v exists, updating. \n", *repo.Name)
+					err := os.Chdir(*repo.Name)
+					cmd := exec.Command("git", "pull", *repo.GitURL)
+					err = cmd.Run()
+					if err != nil {
+						fmt.Printf("Error pulling %v\n", *repo.Name)
+					}
+					// Go one level up
+					os.Chdir("..")
+				} else {
+					fmt.Printf("Cloning %v \n", *repo.Name)
+					cmd := exec.Command("git", "clone", *repo.GitURL)
+					err := cmd.Run()
+					if err != nil {
+						fmt.Printf("Error cloning %v: ", *repo.Name)
+						log.Fatal(err)
+					}
+				}
+			}
+		}
 
-        // Learn about GitHub's pagination here:
-        // https://developer.github.com/v3/
-        if resp.NextPage == 0 {
-            break
-        }
-        opt.ListOptions.Page = resp.NextPage
-    }
+		// Learn about GitHub's pagination here:
+		// https://developer.github.com/v3/
+		if resp.NextPage == 0 {
+			break
+		}
+		opt.ListOptions.Page = resp.NextPage
+	}
 }
