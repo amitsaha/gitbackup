7a42fca32dbd22c91f737c9ddebd24ed697edc83
Amit Saha <amitsaha.in@gmail.com>
2017-03-18 17:56:50 +1100

    release script: create zip for uploading to GitHub

--- release.bash
+++ release.bash
@@ -9,5 +9,10 @@ git tag --annotate v$VERSION -m "Release v$VERSION"
 for pair in linux/386 linux/amd64 linux/arm linux/arm64 darwin/amd64 dragonfly/amd64 freebsd/amd64 netbsd/amd64 openbsd/amd64 windows/amd64; do
 	GOOS=`echo $pair | cut -d'/' -f1`
     GOARCH=`echo $pair | cut -d'/' -f2` 
-    GOOS=$GOOS GOARCH=$GOARCH GOPATH=`pwd`/vendor/  go build -o $DISTDIR/gitbackup-$VERSION-$GOOS-$GOARCH src/gitbackup/main.go src/gitbackup/client.go src/gitbackup/backup.go src/gitbackup/repositories.go
+    OBJECT_FILE="gitbackup-$VERSION-$GOOS-$GOARCH"
+    GOOS=$GOOS GOARCH=$GOARCH GOPATH=`pwd`/vendor/  go build -o "$DISTDIR/$OBJECT_FILE" src/gitbackup/main.go src/gitbackup/client.go src/gitbackup/backup.go src/gitbackup/repositories.go
+    pushd $DISTDIR
+    echo $OBJECT_FILE
+    zip "$OBJECT_FILE.zip" $OBJECT_FILE
+    popd
 done
