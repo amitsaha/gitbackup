cde63f79d55bb6d473b854e1a98a4e1558599b2a
Amit Saha <amitsaha.in@gmail.com>
2019-04-23 14:17:42 +1000

    Add unit test

--- repositories_test.go
+++ repositories_test.go
@@ -52,7 +52,7 @@ func teardown() {
 	server.Close()
 }
 
-func TestGetGitHubRepositories(t *testing.T) {
+func TestGetPublicGitHubRepositories(t *testing.T) {
 	setup()
 	defer teardown()
 
@@ -66,6 +66,25 @@ func TestGetGitHubRepositories(t *testing.T) {
 	}
 	var expected []*Repository
 	expected = append(expected, &Repository{Namespace: "test", CloneURL: "https://github.com/u/r1", Name: "r1", Private: false})
+	if !reflect.DeepEqual(repos, expected) {
+		t.Errorf("Expected %+v, Got %+v", expected, repos)
+	}
+}
+
+func TestGetPrivateGitHubRepositories(t *testing.T) {
+	setup()
+	defer teardown()
+
+	mux.HandleFunc("/user/repos", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprint(w, `[{"full_name": "test/r1", "id":1, "ssh_url": "https://github.com/u/r1", "name": "r1", "private": true}]`)
+	})
+
+	repos, err := getRepositories(GitHubClient, "github", "all", "", "")
+	if err != nil {
+		t.Fatalf("%v", err)
+	}
+	var expected []*Repository
+	expected = append(expected, &Repository{Namespace: "test", CloneURL: "https://github.com/u/r1", Name: "r1", Private: true})
 	if !reflect.DeepEqual(repos, expected) {
 		t.Errorf("Expected %+v, Got %+v", expected, repos)
 	}
