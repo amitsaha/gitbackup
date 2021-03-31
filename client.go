febe53fa50acc638b87f110adec93d2964717961
Amit Saha <amitsaha@users.noreply.github.com>
2021-03-31 12:27:25 +1100

    GitHub - Allow fetching a token via oauth flow (#40)
    * Use github oauth library to add device authentication
    
    Also saves the token in the operating system's secret store.
    
    * update
    
    * update CI files
    
    * Update
    
    * Fix lint issue
    
    * Fix build error
    
    * Switch to go 1.15

--- client.go
+++ client.go
@@ -1,14 +1,74 @@
 package main
 
 import (
+	"context"
+	"fmt"
 	"log"
+	"net/http"
 	"net/url"
 	"os"
 
+	"golang.org/x/oauth2"
+
 	"github.com/google/go-github/v32/github"
 	gitlab "github.com/xanzy/go-gitlab"
-	"golang.org/x/oauth2"
+
+	"github.com/99designs/keyring"
+	"github.com/cli/oauth/device"
 )
+
+var keyringServiceName = "gitbackup-cli"
+var gitbackupClientID = "7b56a77c7dfba0800524"
+
+func startOAuthFlow() string {
+	clientID := gitbackupClientID
+	scopes := []string{"repo", "read:user"}
+	httpClient := http.DefaultClient
+
+	code, err := device.RequestCode(httpClient, "https://github.com/login/device/code", clientID, scopes)
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Printf("Copy code: %s\n", code.UserCode)
+	fmt.Printf("then open: %s\n", code.VerificationURI)
+
+	accessToken, err := device.PollToken(httpClient, "https://github.com/login/oauth/access_token", clientID, code)
+	if err != nil {
+		panic(err)
+	}
+
+	return accessToken.Token
+}
+
+func saveToken(service string, token string) error {
+	ring, err := keyring.Open(keyring.Config{
+		ServiceName: keyringServiceName,
+	})
+	if err != nil {
+		return err
+	}
+
+	err = ring.Set(keyring.Item{
+		Key:  service + "_TOKEN",
+		Data: []byte(token),
+	})
+	return err
+}
+
+func getToken(service string) (string, error) {
+	ring, err := keyring.Open(keyring.Config{
+		ServiceName: keyringServiceName,
+	})
+	if err != nil {
+		return "", err
+	}
+	i, err := ring.Get(service + "_TOKEN")
+	if err != nil {
+		return "", err
+	}
+	return string(i.Data), nil
+}
 
 func newClient(service string, gitHostURL string) interface{} {
 	var gitHostURLParsed *url.URL
@@ -28,13 +88,24 @@ func newClient(service string, gitHostURL string) interface{} {
 	if service == "github" {
 		githubToken := os.Getenv("GITHUB_TOKEN")
 		if githubToken == "" {
-			log.Fatal("GITHUB_TOKEN environment variable not set")
+			githubToken, err = getToken("GITHUB")
+			if err != nil {
+				githubToken = startOAuthFlow()
+			}
+			if githubToken == "" {
+				log.Fatal("GitHub token not available")
+			} else {
+				err := saveToken("GITHUB", githubToken)
+				if err != nil {
+					log.Fatal("Error saving token")
+				}
+			}
 		}
 		gitHostToken = githubToken
 		ts := oauth2.StaticTokenSource(
 			&oauth2.Token{AccessToken: githubToken},
 		)
-		tc := oauth2.NewClient(oauth2.NoContext, ts)
+		tc := oauth2.NewClient(context.Background(), ts)
 		client := github.NewClient(tc)
 		if gitHostURLParsed != nil {
 			client.BaseURL = gitHostURLParsed
--- go.mod
+++ go.mod
@@ -1,11 +1,14 @@
 module github.com/amitsaha/gitbackup
 
-go 1.12
+go 1.15
 
 require (
+	github.com/99designs/keyring v1.1.6
+	github.com/cli/oauth v0.8.0
 	github.com/google/go-github/v32 v32.1.0
 	github.com/mitchellh/go-homedir v1.1.0
 	github.com/spf13/afero v1.2.2
 	github.com/xanzy/go-gitlab v0.16.1
 	golang.org/x/oauth2 v0.0.0-20190402181905-9f3314589c9a
+	golang.org/x/text v0.3.5 // indirect
 )
