e28aba248c907df7f719e45cda3487518b21dc87
Amit Saha <amitsaha.in@gmail.com>
2021-12-31 06:13:17 +1100

    remove unused scripts

--- check_test.bash
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-set -e
-
-export GO111MODULE=on
-go env
-golint -set_exit_status
-go build
-go test
--- release.bash
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-# Stolen from https://github.com/oklog/oklog/blob/master/release.fish
-set -eu
-
-VERSION=$1
-git tag --annotate $VERSION -m "Release $VERSION"
-git push origin $VERSION
