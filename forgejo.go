1ad0b13052179cc1bf044fd40650475ea88e7568
Steve Hoeksema <steve@kotiri.com>
2025-12-30 16:38:22 +1300

    Add pagination

--- forgejo.go
+++ forgejo.go
@@ -28,37 +28,58 @@ func getForgejoRepositories(
 
 		log.Printf("Found user %s with ID %d", user.UserName, user.ID)
 
-		repos, _, err := forgejoClient.SearchRepos(forgejo.SearchRepoOptions{
-			StarredByUserID: user.ID,
+		repos, err := paginateForgejoRepositories(func(page int) ([]*forgejo.Repository, *forgejo.Response, error) {
+			return forgejoClient.SearchRepos(forgejo.SearchRepoOptions{
+				ListOptions:     forgejo.ListOptions{Page: page},
+				StarredByUserID: user.ID,
+			})
 		})
 		if err != nil {
-			log.Fatalf("Error fetching starred repositories from %s: %v", service, err)
+			return nil, fmt.Errorf("fetching starred repositories from %s: %v", service, err)
 		}
 
-		return hydrateForgejoRepositories(repos), nil
+		return repos, nil
 	case "user", "":
-		repos, _, err := forgejoClient.ListMyRepos(forgejo.ListReposOptions{})
+		repos, err := paginateForgejoRepositories(func(page int) ([]*forgejo.Repository, *forgejo.Response, error) {
+			return forgejoClient.ListMyRepos(forgejo.ListReposOptions{
+				ListOptions: forgejo.ListOptions{Page: page},
+			})
+		})
 		if err != nil {
-			log.Fatalf("Error fetching user repositories from %s: %v", service, err)
+			return nil, fmt.Errorf("fetching user repositories from %s: %v", service, err)
 		}
 
-		return hydrateForgejoRepositories(repos), nil
+		return repos, nil
 	default:
 		return nil, fmt.Errorf("unknown repo type: %s", forgejoRepoType)
 	}
 }
 
-func hydrateForgejoRepositories(repos []*forgejo.Repository) []*Repository {
+func paginateForgejoRepositories(fetch func(page int) ([]*forgejo.Repository, *forgejo.Response, error)) ([]*Repository, error) {
 	var repositories []*Repository
+	page := 1
 
-	for _, repo := range repos {
-		repositories = append(repositories, &Repository{
-			CloneURL:  repo.CloneURL,
-			Name:      repo.Name,
-			Namespace: repo.Owner.UserName,
-			Private:   repo.Private,
-		})
+	for {
+		results, resp, err := fetch(page)
+		if err != nil {
+			return nil, err
+		}
+
+		for _, repo := range results {
+			repositories = append(repositories, &Repository{
+				CloneURL:  repo.CloneURL,
+				Name:      repo.Name,
+				Namespace: repo.Owner.UserName,
+				Private:   repo.Private,
+			})
+		}
+
+		if resp == nil || resp.NextPage == 0 {
+			break
+		}
+
+		page = resp.NextPage
 	}
 
-	return repositories
+	return repositories, nil
 }
